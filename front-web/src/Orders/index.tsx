2a03a0682ea1398a3285293011480a46fcc748ec
Rhuan Gonzaga <rhuangonzaga22@gmail.com>
2021-01-08 20:58:48 -0300

    Add Image ,price , description

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
+import {Product} from './types';
 import './styles.css';
+import { fetchProducts } from '../api';
 
 
-function Orders(){
+function Orders() {
+ 
+    const [products, setProducts] = useState<Product[]>([]);
+        console.log(products)
+    useEffect(() => {
+        fetchProducts().then(Response => setProducts(Response.data)).catch(error => console.log(error))
+    }, []);
+    
+    
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
+
         </div>
     )
 
